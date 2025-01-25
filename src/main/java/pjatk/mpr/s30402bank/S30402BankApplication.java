323d86a89f89fc96d789ab69109e9b99d9170117
s30402 <s30402@pjwstk.edu.pl>
2025-01-25 15:43:23 +0100

    Initial commit

---
+++ src/main/java/pjatk/mpr/s30402bank/S30402BankApplication.java
@@ -0,0 +1,13 @@
+package pjatk.mpr.s30402bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S30402BankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(S30402BankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pjatk/mpr/s30402bank/S30402BankApplicationTests.java
@@ -0,0 +1,13 @@
+package pjatk.mpr.s30402bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S30402BankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
